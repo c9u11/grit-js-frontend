f9aa9b4c6d8635ab78cc59b0601d26588812ac34
c9u11 <tjrdud6412@naver.com>
2024-06-16 22:44:05 +0900

    ✨ 회원가입

--- utils/api.js
+++ utils/api.js
@@ -7,3 +7,11 @@ const reqLogin = (email, password) => {
   req.send(JSON.stringify({ email, password }));
   return JSON.parse(req.responseText);
 }
+
+const reqSignup = (username, email, password) => {
+  const req = new XMLHttpRequest();
+  req.open('POST', API_URL + '/signup', false);
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.send(JSON.stringify({ username, email, password }));
+  return JSON.parse(req.responseText);
+}
