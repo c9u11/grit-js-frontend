a2f311eff748e6b574f0b4bce14a58d8c52e0410
c9u11 <tjrdud6412@naver.com>
2024-06-19 23:29:03 +0900

    ✨ 카테고리 페이지 완료

--- utils/api.js
+++ utils/api.js
@@ -15,3 +15,27 @@ const reqSignup = (username, email, password) => {
   req.send(JSON.stringify({ username, email, password }));
   return JSON.parse(req.responseText);
 }
+const reqGetCategories = () => {
+  const req = new XMLHttpRequest();
+  req.open('GET', API_URL + '/categories', false);
+  req.setRequestHeader('authorization', localStorage.getItem('token'));
+  req.send();
+  return JSON.parse(req.responseText);
+}
+
+const reqGetSelectedCategories = () => {
+  const req = new XMLHttpRequest();
+  req.open('GET', API_URL + '/categories/selected', false);
+  req.setRequestHeader('authorization', localStorage.getItem('token'));
+  req.send();
+  return JSON.parse(req.responseText);
+}
+
+const reqPostSelectedCategories = (selectedCategories) => {
+  const req = new XMLHttpRequest();
+  req.open('POST', API_URL + '/categories/selected', false);
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.setRequestHeader('authorization', localStorage.getItem('token'));
+  req.send(JSON.stringify({ selectedCategories }));
+  return JSON.parse(req.responseText);
+}
